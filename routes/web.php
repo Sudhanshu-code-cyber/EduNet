551df47d0e557cd874ff8c081f42d7a09cfad5fe
Saloni Gupta <salonigupta01230@gmail.com>
2025-06-11 12:30:24 +0530

    added

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\TeacherController;
 
 Route::get('/student',[StudentController::class,'dashboard'])->name('/student');
 Route::get('/student/myclass',[StudentController::class,'myclass'])->name('student.myclass');
 Route::get('/student/mytimetable',[StudentController::class,'showTimetable'])->name('student.mytimetable');
 
-Route::get('/teacher/dashboard', function () {
+Route::get('/teacher', function () {
     return view('page.teacher.dashboard');
-});
+})->name('/teacher');
 
 Route::get('teacher/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
+Route::get('/teacher/myclass',[TeacherController::class,'myclass'])->name('teacher.myclass');
+Route::get('/teacher/timetable', [TeacherController::class, 'timetable'])->name('teacher.timetable'); 
