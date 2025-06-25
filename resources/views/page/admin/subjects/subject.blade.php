383cc5c70c06dab73e62db12352ea08d91147b05
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-06-25 11:40:27 +0530

    fix: Remove extra whitespace in subject input field class attribute

--- resources/views/page/admin/subjects/subject.blade.php
+++ resources/views/page/admin/subjects/subject.blade.php
@@ -15,7 +15,7 @@
                 name="name"
                 placeholder="Enter Subject Name"
                 required
-                class="flex-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                class="flex-1 px-4 py-2  border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
             >
             <button
                 type="submit"
